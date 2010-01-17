92609e87f52479fcf8cdbdf47e111b2757c4a65d
Scott Taylor <scott@railsnewbie.com>
2010-01-17 06:02:22 -0500

    Cleanup db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,13 +1,14 @@
 ActiveRecord::Schema.define(:version => 2) do
-  create_table "stories", :force => true do |t|
-    t.string "title", "subtitle"
-    t.string  "type"
-    t.boolean "published"
+  create_table :stories, :force => true do |t|
+    t.string  :title
+    t.string  :subtitle
+    t.string  :type
+    t.boolean :published
   end
 
-  create_table "characters", :force => true do |t|
-    t.integer "story_id"
-    t.string "name"
+  create_table :characters, :force => true do |t|
+    t.integer :story_id
+    t.string  :name
   end
 
   create_table :sessions, :force => true do |t|
